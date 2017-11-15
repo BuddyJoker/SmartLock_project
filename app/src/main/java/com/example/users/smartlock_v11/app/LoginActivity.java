de92276d517464cf4d4ac69ae76804f5ec8f541c
BuddyJoker <505188487@qq.com>
2017-11-15 20:24:42 +0800

    SmartLock_project

---
+++ app/src/main/java/com/example/users/smartlock_v11/app/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.users.smartlock_v11.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.users.smartlock_v11.R;
+
+/**
+ * Created by BigBoss on 2017/11/15.
+ */
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
